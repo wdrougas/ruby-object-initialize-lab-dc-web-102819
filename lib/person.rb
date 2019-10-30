2db7fb169b16b755e2d4fd740ea760ee428d6693
Will Drougas <wdrougas@gmail.com>
2019-10-30 21:22:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,13 +2,14 @@ class Person
   def initialize(@name)
     @name = name
   end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
 end
+  
+#   def name=(name)
+#     @name = name
+#   end
+  
+#   def name
+#     @name
+#   end
+  
+# end
